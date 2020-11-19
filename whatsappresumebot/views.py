6c575d01e56aabab6f64e155be79e4a748a09b5d
vk2607 <vinodkamat98@gmail.com>
2020-11-20 04:48:16 +0530

    csrf exempt

--- whatsappresumebot/views.py
+++ whatsappresumebot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
